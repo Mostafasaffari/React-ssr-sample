e2b7f13a7d45c53e7140e7aac6801f9e48b61ed7
mostafa <mostafasaffari@gmail.com>
2019-09-21 13:57:11 +0430

    fix ts error in window

--- src/container/todo/todoList.tsx
+++ src/container/todo/todoList.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { IDefaultProps } from "../../interfaces/IDefaultProps";
 
+interface CustomWindow extends Window {
+  __initialServerData__?: any;
+}
 interface IProps {
   hello?: string;
 }
 const TodoList: IDefaultProps<IProps> = props => {
+  const windowL: CustomWindow =
+    typeof window !== "undefined" ? window : undefined;
   let initialServerData;
   if (props.staticContext && props.staticContext.initialServerData) {
     initialServerData = props.staticContext.initialServerData.hello;
   } else {
-    initialServerData =
-      typeof window !== "undefined" ? window.__initialServerData__.hello : null;
+    initialServerData = windowL ? windowL.__initialServerData__.hello : null;
   }
-  console.log(initialServerData);
   return <div>TodoList{initialServerData}</div>;
 };
 
