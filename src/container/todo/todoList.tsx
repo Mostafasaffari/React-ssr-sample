c3d737c4ec68785792d61c286ea763d921cd7c7a
mostafa <mostafasaffari@gmail.com>
2019-09-21 21:34:39 +0430

    fix in global window type

--- src/container/todo/todoList.tsx
+++ src/container/todo/todoList.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { IDefaultProps } from "../../interfaces/IDefaultProps";
-
-interface CustomWindow extends Window {
-  __initialServerData__?: any;
+declare global {
+  interface Window {
+    __initialServerData__?: any;
+  }
 }
+
 interface IProps {
   hello?: string;
 }
 const TodoList: IDefaultProps<IProps> = props => {
-  const windowL: CustomWindow =
-    typeof window !== "undefined" ? window : undefined;
   let initialServerData;
   if (props.staticContext && props.staticContext.initialServerData) {
     initialServerData = props.staticContext.initialServerData.hello;
   } else {
-    initialServerData = windowL ? windowL.__initialServerData__.hello : null;
+    initialServerData = window ? window.__initialServerData__.hello : null;
   }
   return <div>TodoList{initialServerData}</div>;
 };
