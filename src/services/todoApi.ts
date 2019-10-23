1da5dac1032396a82e3d0fa645d4bf2583ae8f6c
Mostafa Saffari <mostafasaffari@gmail.com>
2019-10-23 15:05:49 +0330

    fix: separate concern for axios

--- src/services/todoApi.ts
+++ src/services/todoApi.ts
@@ -1,15 +1,13 @@
-import axios from "axios";
+import api from "./axios";
 import { Todo } from "../entities/todo";
 
-axios.defaults.baseURL = "http://localhost:3000/";
-
 const getTodoList = async (): Promise<Todo[]> => {
-  const response = await axios.get("/todos");
+  const response = await api.get("/todos");
   return response.data;
 };
 
 const updateTodoName_api = async (id: string, obj: Todo): Promise<Todo> => {
-  const response = await axios.put(`todos/${id}`, { ...obj });
+  const response = await api.put(`todos/${id}`, { ...obj });
   return response.data;
 };
 
